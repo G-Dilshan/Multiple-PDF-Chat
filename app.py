4be2e903279413476890f868c31f7dde95bf93c6
Dilshan G <133624124+G-Dilshan@users.noreply.github.com>
2025-04-20 20:05:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_conversational_chain():
 
     Answer:
     """
-    llm_model = ChatGoogleGenerativeAI(model='gemini-1.5-pro-001', temperature=0.3)
+    llm_model = ChatGoogleGenerativeAI(model='gemini-1.5-pro-001', temperature=0.9)
 
     prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
     chain = load_qa_chain(llm=llm_model, chain_type='stuff', prompt=prompt)
